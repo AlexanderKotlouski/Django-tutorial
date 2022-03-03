4a098732b58b5be34101806a40888a7f3394d041
alexanderkotlouski <Katlouski@gmail.com>
2022-03-03 15:44:17 +0300

    Lesson1

--- django_tutorial/app_tutorial/views.py
+++ django_tutorial/app_tutorial/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def home(request):
+    return render (request, 'home.html', {'greeting':'Hello'})
+
+
 
 
