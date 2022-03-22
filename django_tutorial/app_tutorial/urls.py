8d1dc2e0b375b608a0deabe885316076531b418f
alexanderkotlouski <Katlouski@gmail.com>
2022-03-22 20:59:12 +0300

    with the changes

--- django_tutorial/app_tutorial/urls.py
+++ django_tutorial/app_tutorial/urls.py
@@ -2,12 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name = 'index'),
-    path('home/', views.home, name = 'index'),
+    path('index/', views.index, name='index'),
     path('book_detail/<int:book_id>/', views.book_detail, name='book_detail'),
     path('book_list/', views.book_list, name='book_list'),
     path('review_detail/<int:review_id>/', views.review_detail, name='review_detail'),
     path('review_list/', views.review_list, name='review_list'),
     path('book_review_list/<int:book_id>/', views.book_review_list, name='book_review_list'),
-    path('reverse/', views.reverse, name = 'reverse'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
+    path("register_user/", views.register_user, name="register_user"),
+    path("login_user/", views.login_user, name="login_user"),
+    path("logout_user/", views.logout_user, name="logout_user"),
+    path('add_review/<int:book_id>/', views.add_review, name='add_review'),
+    path('delete_review/<int:review_id>/', views.delete_review, name='delete_review'),
+    path('edit_review/<int:review_id>/', views.edit_review, name='edit_review'),
+
 ]
--- django_tutorial/app_tutorial/views.py
+++ django_tutorial/app_tutorial/views.py
@@ -1,45 +1,145 @@
+
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Book
-from .models import Review
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import BookForm, EditBookDescriptionForm, ReviewForm, LoginForm, RegisterForm
+from .models import Book, Review
+
+
+@login_required
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            book = Book.objects.create(**form.cleaned_data)
+            return redirect('book_detail', book.id)
+    else:
+        form = BookForm()
+        return render(request, 'form.html', {'form': form})
+
+
+@login_required
+def edit_book(request, book_id):
+    if request.method == 'POST':
+        form = EditBookDescriptionForm(request.POST)
+        if form.is_valid():
+            book = get_object_or_404(Book, pk=book_id)
+            book.description = form.cleaned_data["description"]
+            book.save()
+            return redirect('book_detail', book.id)
+    else:
+        form = EditBookDescriptionForm()
+        return render(request, 'form.html', {'form': form})
+
+
+@login_required
+def add_review(request, book_id):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            book = get_object_or_404(Book, pk=book_id)
+            Review.objects.create(book=book, user=user, **form.cleaned_data)
+            return redirect('book_review_list', book.id)
+    else:
+        form = ReviewForm()
+        return render(request, 'form.html', {'form': form})
+
+
+@login_required
+def delete_review(request, review_id):
+    review = get_object_or_404(Review, pk=review_id)
+    user = request.user
+    if review.user == user:
+        review.delete()
+        return redirect('book_review_list', review.book.id)
+    return HttpResponse('You can delete only your reviews!')
+
+
+@login_required
+def edit_review(request, review_id):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            review = get_object_or_404(Review, pk=review_id)
+            if review.user == user:
+                review.text = form.cleaned_data["text"]
+                review.save()
+                return redirect('book_review_list', review.book.id)
+            return HttpResponse('You can edit only your reviews!')
+    else:
+        form = ReviewForm()
+        return render(request, 'form.html', {'form': form})
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'index.html')
 
-def home(request):
-    return render (request, 'home.html', {'greeting':'Hello'})
 
 def book_detail(request, book_id):
-    #return HttpResponse(f'Your book id is {book_id}')
-    book = Book.objects.get(id=book_id)
+    book = get_object_or_404(Book, pk=book_id)
     return render(request, 'book_detail.html', {'book': book})
 
+
 def book_list(request):
-    #return HttpResponse(f'Your book id is {book_id}')
     books = Book.objects.all()
     return render(request, 'book_list.html', {'books': books})
 
+
 def review_detail(request, review_id):
-    review = Review.objects.get(id=review_id)
+    review = get_object_or_404(Review, id=review_id)
     return render(request, 'review_detail.html', {'review': review})
+
 
 def review_list(request):
     reviews = Review.objects.all()
     return render(request, 'review_list.html', {'reviews': reviews})
 
+
 def book_review_list(request, book_id):
     reviews = Review.objects.filter(book=book_id)
-    return render(request, 'book_review_list.html', {'reviews': reviews})
-
-def reverse(request):
-    user_text = request.GET['usertext']
-    reversed_text = user_text[::-1]
-    return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text})
+    return render(request, 'review_list.html', {'reviews': reviews})
 
 
+def login_user(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect("index")
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'form.html', {'form': form})
 
 
+def logout_user(request):
+    logout(request)
+    return redirect("index")
+
+
+def register_user(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            User.objects.create_user(**form.cleaned_data)
+            return redirect("login_user")
+        else:
+            return HttpResponse('Invalid registration')
+    else:
+        form = RegisterForm()
+
+        return render(request, "form.html", {"form": form})
 
 
 
